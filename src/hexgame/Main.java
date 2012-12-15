05bcc25c0c129349143457efbf93a7d88fd3eab8
Tomasz Pewiński <pewniak747@gmail.com>
2012-12-15 18:56:44 +0100

    first interaction with user

--- src/hexgame/Main.java
+++ src/hexgame/Main.java
@@ -1,11 +1,18 @@
 package org.hexgame;
 
+import java.io.Console;
 import org.hexgame.*;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("WELCOME TO HEX\n");
     Board board = new Board(10);
-    System.out.println(board.toString());
+    Boolean white = true;
+    Console console = System.console();
+    while(true) {
+      System.out.println(board.toString());
+      String name = console.readLine((white ? "WHITE" : "BLACK") + ", what is your move?\n");
+      white = !white;
+    }
   }
 }
