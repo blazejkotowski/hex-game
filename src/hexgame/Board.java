06104b4429582d62af2c64955a185bcbf6f81103
Tomasz Pewiński <pewniak747@gmail.com>
2012-12-15 17:31:49 +0100

    added size assertion

--- src/hexgame/Board.java
+++ src/hexgame/Board.java
@@ -4,6 +4,9 @@ public class Board {
   int size;
 
   public Board(int _size) {
+    if(_size <= 3) {
+      throw new RuntimeException("4 is minimum Board size");
+    }
     size = _size;
   }
 
--- test/BoardTest.java
+++ test/BoardTest.java
@@ -1,9 +1,16 @@
 import org.junit.*;
+import static org.junit.Assert.*;
 import org.hexgame.Board;
 
-public class BoardTest extends junit.framework.TestCase {
+public class BoardTest {
+  @Test
   public void testBoardConstructor() {
     Board board = new Board(4);
     assertTrue(board.getSize() == 4);
+  }
+
+  @Test(expected=RuntimeException.class)
+  public void testMinimalBoardSize() {
+    Board board = new Board(3);
   }
 }
