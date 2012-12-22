3d351503412ce41db3ee6c1a774cd7191fb63609
Tomasz Pewiński <pewniak747@gmail.com>
2012-12-22 23:37:34 +0100

    refactor Board tests

--- test/BoardTest.java
+++ test/BoardTest.java
@@ -4,94 +4,87 @@ import org.hexgame.core.Board;
 import org.hexgame.core.PieceType;
 
 public class BoardTest {
+  Board board;
+
+  @Before
+  public void setUp() {
+    board = new Board(4);
+  }
+
   @Test
   public void testBoardConstructor() {
-    Board board = new Board(4);
     assertEquals(board.getSize(), 4);
   }
 
   @Test(expected=RuntimeException.class)
   public void testMinimalBoardSize() {
-    Board board = new Board(3);
+    board = new Board(3);
   }
 
   @Test
   public void testCanPlacePieceOnBoard() {
-    Board board = new Board(4);
     assertTrue(board.canPlacePiece(1, 1));
   }
 
   @Test
   public void testCanPlacePieceOnLowerPartOfTheBoard() {
-    Board board = new Board(4);
     assertTrue(board.canPlacePiece(7, 4));
   }
 
   @Test
   public void testCannotPlacePieceOffBoard() {
-    Board board = new Board(4);
     assertFalse(board.canPlacePiece(1, 2));
   }
 
   @Test
   public void testCannotPlacePieceOffBoard2() {
-    Board board = new Board(4);
     assertFalse(board.canPlacePiece(2, -1));
   }
 
   @Test
   public void testCannotPlacePieceOffBoard3() {
-    Board board = new Board(4);
     assertFalse(board.canPlacePiece(4, 5));
   }
 
   @Test
   public void testCannotPlacePieceOffBoard4() {
-    Board board = new Board(4);
     assertFalse(board.canPlacePiece(7, 3));
   }
 
   @Test
   public void testCannotPlacePieceOffBoard5() {
-    Board board = new Board(4);
     assertFalse(board.canPlacePiece(7, 5));
   }
 
   @Test
   public void testCannotPlacePieceOffBoard6() {
-    Board board = new Board(4);
     assertFalse(board.canPlacePiece(8, 4));
   }
 
   @Test
   public void testCannotPlacePieceOnOccupiedField() {
-    Board board = new Board(4);
     board.placePiece(1, 1, PieceType.WHITE);
     assertFalse(board.canPlacePiece(1, 1));
   }
 
   @Test
   public void testCannotPlacePieceOnOccupiedField2() {
-    Board board = new Board(4);
     board.placePiece(1, 1, PieceType.BLACK);
     assertFalse(board.canPlacePiece(1, 1));
   }
 
   @Test
   public void testPlacePiece() {
-    Board board = new Board(4);
     board.placePiece(1, 1, PieceType.WHITE);
   }
 
   @Test(expected=RuntimeException.class)
   public void testPlacePieceOutOfBoard() {
-    Board board = new Board(4);
     board.placePiece(1, 3, PieceType.WHITE);
   }
 
   @Test
   public void testRemovePiece() {
-    Board board = new Board(4);
     board.placePiece(1, 1, PieceType.WHITE);
     board.removePiece(1, 1);
     assertEquals(board.getPiece(1, 1), null);
@@ -99,13 +92,11 @@ public class BoardTest {
 
   @Test(expected=RuntimeException.class)
   public void testRemovePieceOutOfBoard() {
-    Board board = new Board(4);
     board.removePiece(1, 3);
   }
 
   @Test
   public void testGetPieces() {
-    Board board = new Board(4);
     board.placePiece(1, 1, PieceType.WHITE);
     board.placePiece(2, 1, PieceType.BLACK);
 
@@ -116,8 +107,6 @@ public class BoardTest {
 
   @Test(expected=RuntimeException.class)
   public void testgetPieceOutOfBoard() {
-    Board board = new Board(4);
     board.getPiece(1, 3);
   }
-
 }
