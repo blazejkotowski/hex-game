3ac0575efbcd09d36ca320813fe8d450fb0063c5
Tomasz Pewiński <pewniak747@gmail.com>
2012-12-22 20:05:01 +0100

    introduce text game controller

--- src/hexgame/core/Main.java
+++ src/hexgame/core/Main.java
@@ -1,36 +1,10 @@
 package org.hexgame;
 
-import java.io.Console;
-import java.util.regex.*;
-import org.hexgame.ui.BoardTextRenderer;
-import org.hexgame.core.*;
+import org.hexgame.ui.TextGameController;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("WELCOME TO HEX\n");
-    Board board = new Board(10);
-    BoardTextRenderer renderer = new BoardTextRenderer(board);
-    PieceType pieceType = PieceType.WHITE;
-    Console console = System.console();
-    while(true) {
-      System.out.println(renderer.toString());
-      Boolean validMove = false;
-      while(!validMove) {
-        String name = console.readLine(pieceType.toString() + ", what is your move?\n");
-        Matcher matcher = Pattern.compile("(\\d+) (\\d+)").matcher(name);
-        int coordinateX;
-        int coordinateY;
-        if(matcher.find()) {
-          coordinateX = Integer.parseInt(matcher.group(1));
-          coordinateY = Integer.parseInt(matcher.group(2));
-          validMove = board.canPlacePiece(coordinateX, coordinateY);
-          if(validMove) {
-            board.placePiece(coordinateX, coordinateY, pieceType);
-          }
-        }
-        if(!validMove) System.out.println("Invalid move!\n");
-      }
-      pieceType = pieceType.invert();
-    }
+    TextGameController gameController = new TextGameController(10);
+    gameController.start();
   }
 }
---
+++ src/hexgame/ui/TextGameController.java
@@ -0,0 +1,43 @@
+package org.hexgame.ui;
+
+import java.io.Console;
+import java.util.regex.*;
+import org.hexgame.core.*;
+import org.hexgame.ui.BoardTextRenderer;
+
+public class TextGameController {
+  Game game;
+  public TextGameController(int size) {
+    Player playerA = new Player(PieceType.WHITE);
+    Player playerB = new Player(PieceType.BLACK);
+    game = new Game(size, playerA, playerB);
+  }
+
+  public void start() {
+    System.out.println("WELCOME TO HEX\n");
+    Board board = new Board(10);
+    BoardTextRenderer renderer = new BoardTextRenderer(board);
+    PieceType pieceType = PieceType.WHITE;
+    Console console = System.console();
+    while(true) {
+      System.out.println(renderer.toString());
+      Boolean validMove = false;
+      while(!validMove) {
+        String name = console.readLine(pieceType.toString() + ", what is your move?\n");
+        Matcher matcher = Pattern.compile("(\\d+) (\\d+)").matcher(name);
+        int coordinateX;
+        int coordinateY;
+        if(matcher.find()) {
+          coordinateX = Integer.parseInt(matcher.group(1));
+          coordinateY = Integer.parseInt(matcher.group(2));
+          validMove = board.canPlacePiece(coordinateX, coordinateY);
+          if(validMove) {
+            board.placePiece(coordinateX, coordinateY, pieceType);
+          }
+        }
+        if(!validMove) System.out.println("Invalid move!\n");
+      }
+      pieceType = pieceType.invert();
+    }
+  }
+}
