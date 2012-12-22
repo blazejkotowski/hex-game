d993604dcf5e4b87c9968232991c55168f85cc92
Tomasz Pewiński <pewniak747@gmail.com>
2012-12-22 19:32:08 +0100

    introduce Player class

---
+++ src/hexgame/core/Player.java
@@ -0,0 +1,10 @@
+package org.hexgame.core;
+
+import org.hexgame.core.PieceType;
+
+public class Player {
+  public PieceType pieceType;
+  public Player(PieceType _pieceType) {
+    pieceType = _pieceType;
+  }
+}
---
+++ test/PlayerTest.java
@@ -0,0 +1,18 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.hexgame.core.PieceType;
+import org.hexgame.core.Player;
+
+public class PlayerTest {
+  @Test
+  public void testInitializingWithWhitePiece() {
+    Player player = new Player(PieceType.WHITE);
+    assertEquals(player.pieceType, PieceType.WHITE);
+  }
+
+  @Test
+  public void testInitializingWithBlackPiece() {
+    Player player = new Player(PieceType.BLACK);
+    assertEquals(player.pieceType, PieceType.BLACK);
+  }
+}
