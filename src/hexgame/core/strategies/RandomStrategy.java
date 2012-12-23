3d02d1c7d0a54f88383b5ecec7d0ca7e5dcd61e3
Tomasz Pewiński <pewniak747@gmail.com>
2012-12-23 16:02:08 +0100

    implement RandomStrategy

--- src/hexgame/core/strategies/RandomStrategy.java
+++ src/hexgame/core/strategies/RandomStrategy.java
@@ -1,5 +1,6 @@
 package org.hexgame.core.strategies;
 
+import java.util.Random;
 import java.util.List;
 import java.util.Arrays;
 import org.hexgame.core.*;
@@ -8,6 +9,7 @@ import org.hexgame.core.strategies.Strategy;
 public class RandomStrategy extends Strategy {
 
   public List<Integer> getMove(Board board, PieceType pieceType) {
-    return Arrays.asList(1, 1);
+    List<List<Integer>> free = freeFields(board);
+    return free.get((new Random()).nextInt(free.size()));
   }
 }
--- src/hexgame/core/strategies/Strategy.java
+++ src/hexgame/core/strategies/Strategy.java
@@ -1,8 +1,20 @@
 package org.hexgame.core.strategies;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 import org.hexgame.core.*;
 
 public abstract class Strategy {
   abstract public List<Integer> getMove(Board board, PieceType pieceType);
+
+  protected List<List<Integer>> freeFields(Board board) {
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+    for(int i = 1; i < 2 * board.getSize(); i++) {
+      for(int l = 1; l <= board.getSize(); l++) {
+        if(board.canPlacePiece(i, l)) result.add(Arrays.asList(i, l));
+      }
+    }
+    return result;
+  }
 }
