1924737ae0e93fea4ad9412dc9c56d57e04a4819
Tomasz Pewiński <pewniak747@gmail.com>
2012-12-23 14:23:28 +0100

    improve TextGameController

--- src/hexgame/ui/TextGameController.java
+++ src/hexgame/ui/TextGameController.java
@@ -10,6 +10,7 @@ public class TextGameController {
   Board board;
   TextBoardRenderer renderer;
   Player currentPlayer;
+  Console console;
 
   public TextGameController(int size) {
     Player playerA = new Player(PieceType.WHITE);
@@ -17,32 +18,81 @@ public class TextGameController {
     game = new Game(size, playerA, playerB);
     board = game.getBoard();
     renderer = new TextBoardRenderer(board);
+    console = System.console();
   }
 
   public void start() {
-    System.out.println("WELCOME TO HEX\n");
-    Console console = System.console();
-    while(true) {
+    greet();
+    while(!game.isFinished()) {
       currentPlayer = game.currentPlayer();
-      System.out.println(renderer.toString());
-      while(true) {
-        String name = console.readLine(currentPlayer.toString() + ", what is your move?\n");
-        Matcher matcher = Pattern.compile("(\\d+) (\\d+)").matcher(name);
-        int xCoordinate = -1;
-        int yCoordinate = -1;
-        if(matcher.find()) {
-          xCoordinate = Integer.parseInt(matcher.group(1));
-          yCoordinate = Integer.parseInt(matcher.group(2));
-        }
-        Move move = new Move(board, currentPlayer, xCoordinate, yCoordinate);
-        if(move.isValid()) {
-          game.performMove(move);
-          break;
-        }
-        else {
-          System.out.println("Invalid move!\n");
-        }
-      }
+      printBoard();
+      askForAction();
     }
+  }
+
+  private void greet() {
+    console.printf("WELCOME TO HEX\n");
+  }
+
+  private void askForAction() {
+    if(currentPlayer.isHuman()) {
+      console.printf(currentPlayer.toString() + ", what is your move?\n");
+      console.printf("[1]: Place a piece\n");
+      console.printf("[2]: Undo a move\n");
+      console.printf("[3]: Show board\n");
+      console.printf("[4]: Exit\n");
+      String move = console.readLine("> ");
+
+      if     (move.equals("1")) placePieceAction();
+      else if(move.equals("2")) undoMoveAction();
+      else if(move.equals("3")) showBoardAction();
+      else if(move.equals("4")) exitAction();
+      else                      defaultAction();
+    }
+    else {
+      // AI action
+    }
+  }
+
+  private void placePieceAction() {
+    console.printf(currentPlayer.toString() + ", where to place a piece?\n");
+    String name = console.readLine("> ");
+    Matcher matcher = Pattern.compile("(\\d+) (\\d+)").matcher(name);
+    int xCoordinate = -1;
+    int yCoordinate = -1;
+    if(matcher.find()) {
+      xCoordinate = Integer.parseInt(matcher.group(1));
+      yCoordinate = Integer.parseInt(matcher.group(2));
+    }
+    Move move = new Move(board, currentPlayer, xCoordinate, yCoordinate);
+    if(move.isValid()) {
+      game.performMove(move);
+    }
+    else {
+      console.printf("Invalid move!\n");
+      placePieceAction();
+    }
+  }
+
+  private void undoMoveAction() {
+    game.undoMove();
+  }
+
+  private void showBoardAction() {
+    printBoard();
+    askForAction();
+  }
+
+  private void exitAction() {
+    System.exit(0);
+  }
+
+  private void defaultAction() {
+    console.printf("Invalid action!\n\n");
+    askForAction();
+  }
+
+  private void printBoard() {
+    console.printf(renderer.toString());
   }
 }
