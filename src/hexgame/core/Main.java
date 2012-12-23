9d56958976768ea349f557f37e4269b874a1b779
Piotr Skonieczny <piotr.skon@gmail.com>
2012-12-24 00:06:26 +0100

    zmiany w main

--- src/hexgame/core/Main.java
+++ src/hexgame/core/Main.java
@@ -1,10 +1,20 @@
 package org.hexgame;
 
 import org.hexgame.ui.TextGameController;
+import org.hexgame.gui.HexGui;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    TextGameController gameController = new TextGameController();
-    gameController.start();
+	System.out.println("1:tryb tekstowy\n 2:tryb graficzny");
+	Scanner sc = new Scanner(System.in);
+	int i = sc.nextInt();
+	if(i == 1){
+    		TextGameController gameController = new TextGameController();
+    		gameController.start();
+	}
+	if(i == 2){
+		HexGui.start();
+	}
   }
 }
